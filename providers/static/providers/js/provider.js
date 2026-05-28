fd0b16c9657a20b57b6204859364f142ae973d56
Sudeep Singh Jadoun <sudeep34861@gmail.com>
2026-05-28 23:31:47 +0530

    provider side bar highleting added

--- providers/static/providers/js/provider.js
+++ providers/static/providers/js/provider.js
@@ -4,3 +4,28 @@ const sidebar = document.querySelector(".provider-sidebar");
 toggleBtn.addEventListener("click", function () {
     sidebar.classList.toggle("collapsed");
 });
+
+
+// const toggleBtn = document.getElementById("sidebarToggle");
+// const sidebar = document.querySelector(".provider-sidebar");
+
+// // ===== LOAD SAVED STATE =====
+// if (localStorage.getItem("sidebarState") === "open") {
+//     sidebar.classList.remove("collapsed");
+// } else {
+//     sidebar.classList.add("collapsed");
+// }
+
+// // ===== TOGGLE SIDEBAR =====
+// toggleBtn.addEventListener("click", function () {
+
+//     sidebar.classList.toggle("collapsed");
+
+//     // SAVE STATE
+//     if (sidebar.classList.contains("collapsed")) {
+//         localStorage.setItem("sidebarState", "closed");
+//     } else {
+//         localStorage.setItem("sidebarState", "open");
+//     }
+
+// });
